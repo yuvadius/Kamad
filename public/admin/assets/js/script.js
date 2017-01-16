674229af460df28b9a565d49042ee7350b75b30a
Yuval Glizerin <yuvalgliz@gmail.com>
2017-01-16 14:38:47 +0200

    Copied hasrata's site

---
+++ public/admin/assets/js/script.js
@@ -0,0 +1,378 @@
+var config = {
+	apiKey: "AIzaSyD5fjRuASD9ZrjZgX315vnDga0pb5DJZ_s",
+	authDomain: "hasrata-724e7.firebaseapp.com",
+	databaseURL: "https://hasrata-724e7.firebaseio.com",
+	storageBucket: "hasrata-724e7.appspot.com",
+	messagingSenderId: "473676323598"
+};
+firebase.initializeApp(config);
+var storageRef = firebase.storage().ref();
+var db = firebase.database();
+
+$.extend(true, $.fn.dataTable.defaults, {
+	dom: 'frtip',
+	colReorder: true,
+	stateSave: false,
+	scrollX: true,
+})
+
+var i18n = new Jed({
+  // Generally output by a .po file conversion
+  locale_data : {
+    "messages" : {
+      "" : {
+        "domain" : "messages",
+        "lang"   : "he",
+        "plural_forms" : "nplurals=2; plural=(n != 1);"
+      },
+      "city" : ["עיר"],
+      "aliahData" : ["תאריך עלייה"],
+      "applications" : ["רמת הידע בבניית אפליקציות לטלפונים חכמים"],
+      "birthData" : ["תאריך לידה"],
+      "buildWeb" : ["רמת השליטה בבניית דפי אינטרנט"],
+      "cellphone" : ["סלולארי"],
+      "citizenship" : ["אזרחות"],
+      "firstName" : ["שם פרטי"],
+      "favoriteCharacter" : ["מה התכונה החזקה או החיובית המאפיינת אותך ביותר? איך זה בא לידי ביטוי?"],
+      "fears" : ["מהם חששותיך מהשירות הצבאי באחת מיחידות ההסרטה?"],
+      "friendsCircle" : ["תאר את חוג החברים שלך"],
+      "ftp" : ["רמת השליטה ברשתות FTP"],
+      "graphicsAnimation" : ["גרפיקה ואנימציה"],
+      "hardPeople" : ["עם איזה אנשים יותר קשה לך להסתדר?"],
+      "hardSchool" : ["מה היה לך יותר קשה בבית-הספר?"],
+      "html5" : ["רמת השליטה ב-HTML5"],
+      "idNumber" : ["תעודת זהות"],
+      "investigateTasrit" : ["תחקיר ותסריט"],
+      "lastName" : ["שם משפחה"],
+      "phone" : ["טלפון"],
+      "networks" : ["ידע בהקמת רשתות מחשבים"],
+      "photoSteel" : ["צילום סטילס"],
+      "prevLastName" : ["שם משפחה קודם"],
+      "production" : ["הפקה"],
+      "scoreAverage" : ["כתוב/י מהו ממוצע הציונים שלך בתעודת הסיום של שנת הלימודים הקודמת"],
+      "serveHasrata" : ["מ-1 עד 5, עד כמה היית מעוניין/ת לשרת באחת מיחידות ההסרטה? (1=הכי נמוך, 5=הכי גבוה)"],
+      "street" : ["רחוב"],
+      "streetNumber" : ["מספר בית"],
+      "userImg" : ["תמונה אישית"],
+      "zip" : ["מיקוד"],
+      "sportsLevel" : ["מ-1 עד 5, מה להערכתך היא רמת הכושר הגופני שלך? (1=הכי נמוך, 5=הכי גבוה)"],
+      "studyStyle" : ["תאר את סגנון הלימוד שלך, איזה תלמיד היית?"],
+      "tnuatNoar" : ["האם היית פעיל בתנועת נוער ו/או פעילות התנדבותית? אם כן, תאר את מעורבותך ומשך הזמן שהיית פעיל"],
+      "tzipiot" : ["מהן ציפיותך מהשירות הצבאי באחת מיחידות ההסרטה?"],
+      "videoEditing" : ["עריכת וידאו"],
+      "videoShoot" : ["צילום וידאו, תאורה וסאונד"],
+      "whatDoIf" : ["במידה ותחוש שאתה לא ממצה את כישוריך, מה תעשה?"],
+      "whatSpecialGive" : ["מה לדעתך תוכל לתרום במיוחד ליחידת ההסרטה?"],
+      "whatWillGive" : ["מה לדעתך השירות ביחידת ההסרטה יוכל להקנות לך?"],
+      "whyServeHasrata" : ["אני מעוניין/ת לשרת מיחידות ההסרטה כי..."],
+      "worstCharacter" : ["איזו תכונה חלשה או שלילית מאפיינת אותך? איך זה בא לידי ביטוי?"],
+      "yourTeacherDesc" : ["איך המחנך שלך היה מתאר אותך?"],
+      "motivationRadio" : ["ביחס לתפקידים אחרים/יחידות אחרות בהם היית רוצה לשרת בצבא, היכן מדורגות יחידות ההסרטה?"],
+      "notes" : ["הערות"],
+      "score" : ["ציון"],
+      "skill" : ["מקצוע"],
+      "unitsNumber" : ["	מספר יחידות"],
+      "age" : ["גיל"],
+      "name" : ["שם"],
+      "fromYear" : ["משנה"],
+      "toYear" : ["עד שנה"],
+      "leftReason" : ["סיבת עזיבה"],
+      "howLearned" : ["איך למדתי"],
+      "softwareName" : ["תוכנה/ציוד"],
+      "time" : ["זמן שימוש/היכרות"],
+      "whatKnow" : ["מה אני יועד ליישם"],
+      "file" : ["קובץ"],
+      "details" : ["שם עבודה, שנה ופרטיים כללים"],
+      "rule" : ["תפקיד שביצעתי\\תרומתי לעבודה"],
+      "type" : ["מדיה"],
+      "2units" : ["אם תתקבל/י לאחת מהיחידות, באילו שניים מהתחומים הבאים היית הכי רוצה לעסוק?"],
+      "langName" : ["שפה"],
+      "langLevel" : ["רמה"],
+      "limitName" : ["מגבלה"],
+      "limit" : ["תאור המגבלה"],
+      "conditions" : ["תיאור מגבלת תנאי שירות"],
+      "5unitsPlus" : ["ציון מקצועות נוספים של 5 יחידות ומעלה"],
+      "armyLimit" : ["מלא\י מגבלות שירות ושיבוץ צבאי"],
+      "communicationScore" : ["ציוני מקצועות התקשורת"],
+      "familyDetails" : ["מלא\י פרטים אודות משפחתך הגרעינית"],
+      "portfolioFiles" : ["קבצים תיק עבודות"],
+      "school" : ["בית ספר"],
+      "softwares" : ["תוכנות\\ציוד מקצועי בהם אני משתמש"],
+      "languages" : ["פרט/י בנוגע לכישורי שפה"],
+      "type" : ["מדיה"],
+      "miclala" : ["האם את/ה בלימודי מכללה במסלול קולנוע וטלוויזיה (י״ג-י״ד)?"],
+      "gender" : ["מין"],
+      "bagrut" : ["ככל הנראה, האם תסיים/י את התיכון עם תעודת בגרות מלאה?"],
+      "units": ["שני תחומים מועדפים בהסרטה"]
+    }
+  },
+  "domain" : "messages"
+});
+
+$(document).ready(function() {
+	db.ref('users').once('value', function(snapshot) {
+		users = snapshot.val()
+		rows = []
+		columns = []
+		$.each(users, function(uid, user) {
+			rows.push(user)
+			if(user.userImg !== undefined)
+				user.userImg = '<a href="#" class="link">'+user.userImg+'</a>'
+			$.each(user, function(index, value) {
+				if(typeof value !== 'object')
+					columns.push(index)
+				else {
+					counter = 1
+					$.each(value, function(i, obj) {
+						$.each(obj, function(idx, val) {
+							user[idx+counter] = val
+							columns.push(idx+counter)
+						})
+						counter++
+					})
+				}
+			})
+		})
+
+		cols = [{defaultContent: ""}]
+		colsNoArrays = [{defaultContent: ""}]//Doesn't contain list columns like files and notes
+		specialCols = ["idNumber", "firstName", "lastName", "2units"]
+		columns = $.unique(columns)
+		$.each(specialCols, function(index, column) {
+			colsNoArrays.push({
+				title: i18n.gettext(column.replace(/[0-9]/g, '')),
+				name: column,
+				data: column,
+				defaultContent: ""
+			})
+		})
+		$.each(columns, function(index, column) {
+			if(column != 'uid') {
+				cols.push({
+					title: i18n.gettext(column.replace(/[0-9]/g, '')),
+					name: column,
+					data: column,
+					defaultContent: ""
+				})
+				if((!column.match(/.*\d$/gm) || column == 'html5') && jQuery.inArray(column, specialCols) == -1) {
+					colsNoArrays.push({
+						title: i18n.gettext(column.replace(/[0-9]/g, '')),
+						name: column,
+						data: column,
+						defaultContent: ""
+					})
+				}
+			}
+		})
+		console.log(colsNoArrays)
+		table = $('#users').DataTable({
+			dom: 'Bfrtip',
+		    scrollY: '55vh',
+		    scroller: true,
+	        buttons: [
+	            'colvis',
+				'selectAll',
+				'selectNone',
+		        {
+	                text: 'Delete',
+	                action: function ( e, dt, node, config ) {
+	                	rows = table.rows('.selected')
+	                	if(Object.keys(table.rows('.selected')[0]).length > 0) {
+							if (confirm('למחוק בטוח את כל השורות שנבחרו?')) {
+								$.each(rows.data(), function(index, row) {
+									uid = row.uid
+									files = []
+									if(row.userImg !== undefined)
+										files.push(row.userImg.replace('<a href="#" class="link">', '').replace('</a>', ''))
+									$.each(row.portfolioFiles, function(i, obj) {
+										files.push(obj.file)
+									})
+									db.ref('users/'+uid).set(null, function() {
+										rows.remove().draw()
+										$.each(files, function(index, fileName) {
+											storageRef.child('files/'+uid+'/'+fileName.replace('<a href="#" class="link">', '').replace('</a>', '')).delete();
+										})
+									})
+								})
+							}
+	                	}
+	                   
+	                }
+	            },
+	            {
+                	extend: 'csv',
+	                exportOptions: {
+	                    modifier: {
+	                        selected: true
+	                    }
+	                }
+	            }
+	        ],
+		    columnDefs: [ {
+	            orderable: false,
+	            className: 'select-checkbox',
+	            targets:   0
+	        } ],
+	        select: {
+	            style:    'multi',
+	            selector: 'td:first-child'
+	        },
+			columns: colsNoArrays
+		})
+
+		tableUser = $('#tableUser').DataTable({
+		    scrollY: '40vh',
+			columns: [
+				{
+					title: 'index',
+					name: 'index',
+					data: 'index',
+					defaultContent: ""
+				},
+				{
+					title: 'value',
+					name: 'value',
+					data: 'value',
+					defaultContent: ""
+				}
+			],
+			bPaginate: false,
+			scrollX: false,
+			scroller: false,
+    		rowReorder: true
+		})
+
+		table.rows.add(rows).draw()
+
+		$(document).on('click', '#users tr td:not(.select-checkbox)', function() {
+			$("#tables").empty()
+			$('#modal1').openModal();
+			$("#content").hide()
+			$("#loading").show()
+
+			setTimeout(function(t) {
+				data = table.row($(t)).data()
+				//Deletes all array data from the table "tableUser"
+				toDelete = []
+				$.each(data, function(index, value) {
+					if(index.match(/.*\d$/gm) && index != 'html5') {
+						toDelete.push(index)
+					}
+				})
+				for (var i = 0; i < toDelete.length; i++) {
+					delete data[toDelete[i]]
+				}
+
+				$("#personalImg").attr('src', '')
+				$("#uid").val(data.uid)
+				img = data.userImg.replace('<a href="#" class="link">', '').replace('</a>', '')
+				storageRef.child('files/'+$("#uid").val()+'/'+img).getDownloadURL().then(function(url) {
+					console.log(url)
+					$("#personalImg").attr('src', url)
+				})
+				rows = []
+				$.each(data, function(index, value) {
+					if(typeof value !== 'object') {
+						if(index != 'uid') {
+							rows.push({
+								'index': i18n.gettext(index.replace(/[0-9]/g, '')),
+								'value': value
+							})
+						}
+					}
+					else {
+						$("#tables").append("<hr><h5>"+i18n.gettext(index)+"</h5><table id='"+index+"' class='table table-striped table-bordered' cellspacing=''0' width='100%'></table>")
+						columnsNames = []
+						rows2 = []
+						$.each(value, function(idx, val) {
+							if(index == 'portfolioFiles') {
+								$.each(val, function(i, n) {
+									if(i == 'file')
+										value[idx][i] = '<a href="#" class="link">'+n+'</a>'
+								})
+							}
+							if(index == 'armyLimit')
+								val['limitName'] = i18n.gettext(idx)
+							rows2.push(val)
+						})
+						$.each(flatten(value), function(index, column) {
+							name = /[^\.]*$/.exec(index)[0]
+							columnsNames.push(name)
+						})
+						columnsNames = $.unique(columnsNames)
+						columns = []
+						$.each(columnsNames, function(index, name) {
+							columns.push({
+								title: i18n.gettext(name),
+								name: name,
+								data: name,
+								defaultContent: ""
+							})
+						})
+						window[index] = $("#"+index).DataTable({
+							columns: columns,
+							scrollX: false,
+							scroller: false,
+							bPaginate: false
+						})
+
+						window[index].clear()
+						window[index].rows.add(rows2).draw()
+					}
+					tableUser.clear()
+					tableUser.rows.add(rows).draw()
+					$("#loading").hide()
+					$("#content").show()
+				})
+			}, 500, this)
+		})
+	})
+})
+
+$(document).on('click', '.link', function() {
+	storageRef.child('files/'+$("#uid").val()+'/'+$(this).text()).getDownloadURL().then(function(url) {
+		win = window.open(url, '_blank');
+  		win.focus();
+	})
+})
+
+function isBuffer (obj) {
+	return !!obj.constructor && typeof obj.constructor.isBuffer === 'function' && obj.constructor.isBuffer(obj)
+}
+
+function flatten(target, opts) {
+  opts = opts || {}
+
+  var delimiter = opts.delimiter || '.'
+  var maxDepth = opts.maxDepth
+  var output = {}
+
+  function step(object, prev, currentDepth) {
+    currentDepth = currentDepth ? currentDepth : 1
+    Object.keys(object).forEach(function(key) {
+      var value = object[key]
+      var isarray = opts.safe && Array.isArray(value)
+      var type = Object.prototype.toString.call(value)
+      var isbuffer = isBuffer(value)
+      var isobject = (
+        type === "[object Object]" ||
+        type === "[object Array]"
+      )
+
+      var newKey = prev
+        ? prev + delimiter + key
+        : key
+
+      if (!isarray && !isbuffer && isobject && Object.keys(value).length &&
+        (!opts.maxDepth || currentDepth < maxDepth)) {
+        return step(value, newKey, currentDepth + 1)
+      }
+
+      output[newKey] = value
+    })
+  }
+
+  step(target)
+
+  return output
+}
---
+++ public/assets/js/script.js
@@ -0,0 +1,350 @@
+var isMobile = false; //initiate as false
+// device detection
+if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) 
+    || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) {
+	window.location = "mobile.html";
+}
+
+var config = {
+	apiKey: "AIzaSyD5fjRuASD9ZrjZgX315vnDga0pb5DJZ_s",
+	authDomain: "hasrata-724e7.firebaseapp.com",
+	databaseURL: "https://hasrata-724e7.firebaseio.com",
+	storageBucket: "hasrata-724e7.appspot.com",
+	messagingSenderId: "473676323598"
+};
+firebase.initializeApp(config);
+var storageRef = firebase.storage().ref();
+var db = firebase.database();
+
+var debug;
+var uid = guid()
+
+$(document).ready(function() {
+	$('input').click(function() {
+		if($(this).hasClass('warning'))
+	  		$(this).removeClass('warning')
+	});
+
+	$('textarea').click(function() {
+		if($(this).hasClass('warning'))
+	  		$(this).removeClass('warning')
+	});
+
+	$('label').click(function() {
+		if($(this).hasClass('warning'))
+	  		$(this).removeClass('warning')
+	});
+
+	$(".buttons .button").click(function() {
+		id = $(this).attr("id")
+		currentStep = Number($(".content .step:visible").attr("class").replace("step step", ""))
+		if(id == 'continue') {
+			x = currentStep+1
+			$( ".step" + currentStep + " .mandatory" ).each(function( index ) {
+			  if(!$.trim(this.value).length) { // zero-length string AFTER a trim
+				x = currentStep
+				if(this.id == "userImg")
+					$("#userImgLabel").addClass('warning')
+				else
+					$(this).addClass('warning')
+			  }
+			});
+			if(currentStep == 3){
+				counter = 0;
+				$( ".step3 .2units" ).each(function( index ) {
+				  counter += this.checked
+				});
+				if(counter != 2) {
+					x = currentStep
+				}
+			}
+		}
+		else
+			x = currentStep-1
+
+		changeStep(x)
+	})
+
+	$(document).on('click', '.colored', function() {
+		number = Number($(this).find(".img").attr('class').replace('img icon', ''))
+		changeStep(number)
+	})
+
+	$('.empty').prepend('<option value=""></option>')
+
+	for(i = 1; i <= 31; i++) {
+		$('.days').append('<option value="' + i + '">' + i + '</option>');
+	}
+
+	var months = ["ינואר", "פברואר", "מרץ", "אפריל", "מאי", "יוני", "יולי", "אוגוסט", "ספטמבר", "אוקטובור", "נובמבר", "דצמבר"];
+	for(i = 1; i <= 12; i++) {
+		$('.months').append('<option value="' + i + '">' + months[i-1] + '</option>');
+	}
+
+	$('.years').append(getYearsOptions())
+	$('select.languages').append(getLanguagesOptions())
+
+	/*$('.languages.english').val('אנגלית')
+	$('.languages.arabic').val('ערבית')
+	$('.languages.french').val('צרפתית')*/
+
+	var citizenships = ["", "אוזבקית","אוקראינית","אורדו","אזרבייג'נית","איטלקית","אינדונזית","איסלנדית","אלבנית","אמהרית","אנגלית","אסטונית","אפריקאנס","אקאן","ארמנית","בולגרית","בוסנית","בורמזית","בלרוסית","בנגלית","גוג'ראטית","גרוזינית","גרמנית","דזונקה","דנית","הולנדית","הונגרית","הינדי","המונג","וולשית","וייטנאמית","זולו","חמרית","טג'יקית","טגלוג","טורקית","טורקמנית","טיבטית","טלוגו","טמילית","טסוואנה","יאו","יאוית","יוונית","יידיש","יפנית","כורדית","לאו","לטבית","לטינית","ליטאית","לינגלה","מונגולית ח'לח'ית","מלאית","מלגשית","מלטזית","מנדרינית","מקדונית","מראטהי","מרשלזית","נוארית","נורווגית","נייאנג'ה","נפאלית","סווהילית","סוטו","סומאלית","סינהלה","סינית, מסורתית","סינית, מפושטת","סלובנית","סלובקית","סמואית","ספרדית","סרבית","עברית","ערבית","פוג'ואו","פולנית","פורטוגזית","פינית","פלמית","פנג'אבי","פרסית","פשטו","צ'וקסית","צ'כית","צרפתית","קאנדה","קוריאנית","קזחית","קטלנית","קירונדי","קנטונזית","קצ'ואה","קרואטית","קריאולית האיטית","רומאני","רומאנש","רומנית","רוסית","שבדית","שנחאית","תאילנדית","תיגרינית"]
+	for(i = 1; i <= citizenships.length; i++) {
+		$('.citizenships').append('<option value="' + citizenships[i-1] + '">' + citizenships[i-1] + '</option>');
+	}
+
+    $("input[type='checkbox']").change(function() {
+		if(this.checked) {
+			$("." + $(this).attr('id')).removeAttr("disabled");
+		}
+		else {
+			$("." + $(this).attr('id')).attr("disabled", "");
+		}
+    })
+
+	$("#userImg").change(function(e) {
+		var file = $(this)[0].files[0]
+		if(file !== undefined)
+			$("#imgName").text(file.name)
+	});
+
+	$(document).on('click', '.files_form3 .remove', function() {
+		$(this).closest('tr:not(.th)').remove()
+	})
+
+	var limit = 2;
+	$('input[name="2units"]').on('change', function(evt) {
+	   if($(this).siblings(':checked').length >= limit) {
+	       this.checked = false;
+	   }
+	});
+
+	$(document).on('click', '.addRemovePlugin .disabled .add', function() {
+		$(this).closest(".addRemovePlugin").append($(this).closest('.disabled').clone().addClass('new'))
+		$(this).closest('.addRemovePlugin').find('.disabled:not(.new)').removeClass('disabled').find(".plusRemoveIcon").addClass('remove').removeClass('add').attr('src', 'assets/img/remove.png')		
+		newTr = $(this).closest('.addRemovePlugin').find('.new').removeClass('new')
+		//These lines create the NaN bug. I don't think that they are necessary
+		/*name = newTr.find('input').attr('name')
+		if(name !== undefined)
+			newTr.find('input').attr('name', name.split('_')[0] + (Number(name.split('_')[1]) + 1))*/
+
+	})
+
+	$(document).on('click', '.addRemovePlugin .remove', function() {
+		$(this).closest("tr").remove()
+	})
+
+	$("input[type='range']:not(.five)").ionRangeSlider({
+        type: "single",
+        grid: true,
+        min: 1,
+        max: 10,
+        step: 1,
+		values: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    })
+    $("input[type='range'].five").ionRangeSlider({
+        type: "single",
+        grid: true,
+        min: 1,
+        max: 5,
+        step: 1,
+		values: [1, 2, 3, 4, 5]
+    })
+})
+
+function getYearsOptions() {
+	var year = new Date().getFullYear();
+	output = ''
+	for(i = year; i >= year - 30; i--) {
+		output += '<option value="' + i + '">' + i + '</option>';
+	}
+	return output
+}
+
+function getLanguagesOptions() {
+	var year = new Date().getFullYear(),
+		output = '',
+		languages = ["", "אוזבקית","אוקראינית","אורדו","אזרבייג'נית","איטלקית","אינדונזית","איסלנדית","אלבנית","אמהרית","אנגלית","אסטונית","אפריקאנס","אקאן","ארמנית","בולגרית","בוסנית","בורמזית","בלרוסית","בנגלית","גוג'ראטית","גרוזינית","גרמנית","דזונקה","דנית","הולנדית","הונגרית","הינדי","המונג","וולשית","וייטנאמית","זולו","חמרית","טג'יקית","טגלוג","טורקית","טורקמנית","טיבטית","טלוגו","טמילית","טסוואנה","יאו","יאוית","יוונית","יידיש","יפנית","כורדית","לאו","לטבית","לטינית","ליטאית","לינגלה","מונגולית ח'לח'ית","מלאית","מלגשית","מלטזית","מנדרינית","מקדונית","מראטהי","מרשלזית","נוארית","נורווגית","נייאנג'ה","נפאלית","סווהילית","סוטו","סומאלית","סינהלה","סינית, מסורתית","סינית, מפושטת","סלובנית","סלובקית","סמואית","ספרדית","סרבית","עברית","ערבית","פוג'ואו","פולנית","פורטוגזית","פינית","פלמית","פנג'אבי","פרסית","פשטו","צ'וקסית","צ'כית","צרפתית","קאנדה","קוריאנית","קזחית","קטלנית","קירונדי","קנטונזית","קצ'ואה","קרואטית","קריאולית האיטית","רומאני","רומאנש","רומנית","רוסית","שבדית","שנחאית","תאילנדית","תיגרינית"]
+
+	for(i=0; i<languages.length; i++) {
+		output += '<option value="'+languages[i]+'">' + languages[i] + '</option>';
+	}
+	return output
+}
+
+function changeStep(x) {
+	for(i=1; i<=10; i++) {
+		$(".line").removeClass("colored")
+	}
+	if(x >= 1) {
+		for(i=1; i<=x; i++) {
+			$(".line"+i).addClass("colored")
+		}
+	}
+
+	if(x > 0 && x < 10)
+		$(".buttons .back").show()
+	else
+		$(".buttons .back").hide()
+	if(x < 9)
+		$(".buttons .continue").show()
+	else
+		$(".buttons .continue").hide()
+
+	$(".content .step").hide()
+	$(".content .step"+x).show()
+
+	if(x == 9)
+		uploadFiles()
+}
+
+function uploadFiles() {
+	var inputs = $("input[type='file']")
+	var files = []
+	$.each(inputs, function(index, input) {
+		$.each(input.files, function(index, file) {
+			files.push(file)
+		})
+	})
+
+	$(".filesUpload").empty()
+	var total = files.length
+	var count = 0
+	$.each(files, function(index, file) {
+		var uploadTask = storageRef.child('files/'+uid+'/'+file.name).put(file)
+
+		newTr = '<tr>' +
+                    '<td>'+file.name+'</td>' +
+                    '<td><div class="percent"></div><progress id="'+file.name+'" value="0" max="100"></progress></td>' +
+                '</tr>'
+
+		$(".filesUpload").append(newTr)
+
+		uploadTask.on('state_changed', function(snapshot){
+		  var progress = Math.floor((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
+		  $("progress[id='"+file.name+"']").val(progress)
+		  $("progress[id='"+file.name+"']").parent().find('.percent').text(progress + '%')
+
+		  switch (snapshot.state) {
+		    case firebase.storage.TaskState.PAUSED: // or 'paused'
+		      //console.log('Upload is paused');
+		      break;
+		    case firebase.storage.TaskState.RUNNING: // or 'running'
+		      //console.log('Upload is running');
+		      break;
+		  }
+		}, function(error) {
+		  // Handle unsuccessful uploads
+		}, function() {
+			//console.log('Uploaded a blob or file!');
+			var downloadURL = uploadTask.snapshot.downloadURL;
+			//console.log(downloadURL)
+			count++
+			if(total == count)
+				sendForm()
+		});
+	})
+}
+
+function sendForm() {
+	form = {}
+	$.each($(".step1 input[type='text'], .step1 select, .step1 input[type='radio']:checked"), function(index, input) {
+		id = $(input).attr('id')
+		name = $(input).attr('name')
+		if(id !== undefined)
+			form[id] = $(input).val()
+		else if(name !== undefined)
+			form[name] = $(input).val()
+	})
+
+	$.each($(".input.date"), function(index, element) {
+		form[$(this).attr('id')] = $(element).find("select.days").val() +'/'+ $(element).find("select.months").val() +'/'+ $(element).find("select.years").val()
+	})
+
+	$.each($("table:not(.filesUpload)"), function(index, table) {
+		if($(table).attr('id') !== undefined) {
+			form[$(table).attr('id')] = []
+			$.each($(table).find("tr:not(.disabled):not(.th)"), function(index, tr) {
+				obj = {}
+				if($(table).attr('id') == 'languages') {
+					input = $(tr).find(":input[type='radio']:checked")
+					if(input.length) {
+						obj[input.attr('name').split('_')[0]] = input.val()
+						obj['langName'] = $(tr).find(".firsttd").text()
+						if(obj['langName'] == '')
+							obj['langName'] = $(tr).find("select").val()
+					}
+				}
+				else {
+					$.each($(tr).find("td:not(:has(> img))"), function(index, td) {
+						input = $(td).find(":input")
+						value = input.val()
+						if(input.attr('type') == 'file')
+							value = value.split('/').pop().split('\\').pop()
+						obj[input.attr('name')] = value
+					})
+				}
+				if(!$.isEmptyObject(obj))
+					form[$(table).attr('id')].push(obj)
+			})
+		}
+	})
+
+	form['armyLimit'] = {}
+	$.each($("#armyLimit").find("input[type='checkbox']:checked"), function(index, checkbox) {
+		box = $(checkbox).closest(".input.innerBox2in2")
+		text = box.attr('id')
+		form['armyLimit'][text] = {}
+		$.each(box.find(":input[type!='checkbox']"), function(index, input) {
+			form['armyLimit'][text][$(input).attr('name')] = $(input).val()
+		})
+	})
+
+	form['2units'] = []
+	$.each($("input[name='2units']:checked"), function(index, input) {
+		form['2units'].push($(input).val())
+	})
+	form['2units'] = form['2units'].join()
+
+	$.each($("input[type='radio']:checked"), function(index, input) {
+		if($(input).closest('table').length) {
+			if($(input).closest('table').attr('id') == 'languages')
+				return
+		}
+		form[$(input).attr('name')] = $(input).val()
+	})
+
+	$.each($(":input[name='alone']"), function(index, input) {
+		value = $(input).val()
+		if($(input).attr('max') !== undefined && $(input).attr('type') == 'range')
+			value = value + '/' + $(input).attr('max')
+		form[$(input).attr('id')] = value
+	})
+
+	$.each($(":input[type!='radio'][name!='2units']:checked"), function(index, input) {
+		form[$(input).attr('name')] = $(input).val()
+	})
+
+	form['userImg'] = $("#userImg").val().split('/').pop().split('\\').pop()
+	form['uid'] = uid
+	delete form['undefined']
+	for(i=0;i<form['portfolioFiles'].length;i++) {
+		delete form['portfolioFiles'][i]['undefined']
+	}
+	console.log(form)
+	db.ref('users/'+uid).set(form, function() {
+		changeStep(10)
+		console.log(form)
+	})
+}
+
+function guid() {
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+    s4() + '-' + s4() + s4() + s4();
+}
+
+function s4() {
+  return Math.floor((1 + Math.random()) * 0x10000)
+    .toString(16)
+    .substring(1);
+}
